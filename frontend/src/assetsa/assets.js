446de1b24fdfbe6f6d6ce7c293df28eb6c79ac6e
Abtin-coder <wpabtin@gmail.com>
2025-11-17 00:07:40 +0330

    404 error
    404error

--- frontend/src/assetsa/assets.js
+++ frontend/src/assetsa/assets.js
@@ -1,9 +1,11 @@
 import logo from './logo.png';
-import cross_icon from './cross_icon.svg'
-import menu_icon from './menu_icon.svg'
+import cross_icon from './cross_icon.svg';
+import menu_icon from './menu_icon.svg';
+import error404 from './error404.png'; 
 
 export const assets = {
     logo,
     cross_icon,
     menu_icon,
-}
+    error404, 
+};
