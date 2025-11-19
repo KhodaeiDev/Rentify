6c528e3677461b66d479b902fb3495bbd3870ed4
Abtin-coder <wpabtin@gmail.com>
2025-11-19 23:34:10 +0330

    about us
    finished about us / contact us

--- frontend/src/assetsa/assets.js
+++ frontend/src/assetsa/assets.js
@@ -2,10 +2,18 @@ import logo from './logo.png';
 import cross_icon from './cross_icon.svg';
 import menu_icon from './menu_icon.svg';
 import error404 from './error404.png'; 
+import email from './email.png'
+import location from './location.png'
+import time from './time.png'
+import aboutus from './aboutus.png'
 
 export const assets = {
     logo,
     cross_icon,
     menu_icon,
     error404, 
+    email,
+    location,
+    time,
+    aboutus
 };
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "../components/Layout";
 import Home from "../pages/Home";
 import Error404 from "../pages/error404";
+import AboutUS from "../pages/AboutUS";
 
 export default function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ export default function AppRoutes() {
       <Route path="/" element={<Navigate to="/home" replace />} />
       <Route element={<Layout />}>
         <Route path="/home" element={<Home />} />
+        <Route path="/aboutus" element={<AboutUS/>}/>
         <Route path="*" element={<Error404/>}/>
       </Route>
     </Routes>
